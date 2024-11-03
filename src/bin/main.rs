5ea902044e92738fd2d98558bdbb2fc1442aaf01
nkconnor <nicholas.k.connor@gmail.com>
2024-11-02 21:34:26 -0400

    Added arg parser (clap) and made -p/--port an argument to bin/main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,20 @@
 use programmatic_go::listener;
 use std::{error::Error, net::SocketAddr};
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Port to run the server on
+    #[arg(short, long)]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let args = Args::parse();
+    let addr = SocketAddr::from(([127, 0, 0, 1], args.port));
     listener::start(addr).await?;
     Ok(())
 }
